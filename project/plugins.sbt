9463c906b0203da4724723d094f4731e794fb17c
Jose Labra <jelabra@gmail.com>
2022-01-06 14:31:26 +0100

    First version of gh-pages

--- project/plugins.sbt
+++
@@ -1,1 +0,0 @@
-addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
--- src/main/scala/es/weso/wb/shapeOpt.scala
+++
@@ -1,9 +0,0 @@
-package es.weso.wb
-import com.monovore.decline._
-
-object ShapeOpt {
-  val shape = Opts.option[String](
-   "shape", 
-   "Shape to validate in entity schema (if not specified, it uses Start shape)")
-  .orNone
-}
